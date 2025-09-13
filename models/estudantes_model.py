6c89d07ff284b2d3281413ad7c9b3ba171f00daa
Víh Albuquerque <ti.vih498@outlook.com>
2025-09-12 22:10:04 -0300

    Add files via upload

--- models/estudantes_model.py
+++ models/estudantes_model.py
@@ -1,8 +1,12 @@
 class Estudante:
-    def __init__(self, matricula, nome, sexo):
+    def __init__(self, matricula, nome, sexo, nota1=None, nota2=None, media=None, status=None):
         self.matricula = matricula
         self.nome = nome
         self.sexo = sexo
+        self.nota1 = nota1
+        self.nota2 = nota2
+        self.media = media
+        self.status = status
 
     def __repr__(self):
-        return f"Estudante({self.matricula}, {self.nome}, {self.sexo})"
+        return f"Estudante({self.matricula}, {self.nome}, {self.sexo}, {self.nota1}, {self.nota2}, {self.media}, {self.status})"
