a652bd6259f4fbe51ee09c1d3160836d556402ca
Víh Albuquerque <ti.vih498@outlook.com>
2025-09-12 22:10:19 -0300

    Delete models/conteudo_model.py

--- models/conteudo_model.py
+++
@@ -1,8 +0,0 @@
-class Conteudo:
-    def __init__(self, id, titulo, descricao):
-        self.id = id
-        self.titulo = titulo
-        self.descricao = descricao
-
-    def __repr__(self):
-        return f"Conteudo({self.id}, {self.titulo}, {self.descricao})"
