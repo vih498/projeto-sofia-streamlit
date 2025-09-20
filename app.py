384a4dce0b1ae3730b60fec5352b99d2e2f91a5a
Víh Albuquerque <ti.vih498@outlook.com>
2025-09-20 13:17:26 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,40 +1,45 @@
 import streamlit as st
-from controllers.estudante_controller import init_db
 from controllers.estudante_controller import init_db, atualizar_tabela
+from views import login, cadastro_estudantes, dashboard_escolar
+from views import conteudo_ia, tradutor, analise_documentos
 
-
-# ---------- INICIALIZAÇÃO DO BANCO ----------
-init_db()  # garante que a tabela 'estudante' exista antes de qualquer consulta
+init_db()
 atualizar_tabela()
 
-# ---------- TÍTULO PRINCIPAL ----------
-st.markdown(
-    '<h1 style="color:black; font-size:75px;">Projeto SofIA - 498</h1>', 
-    unsafe_allow_html=True
-)
+st.markdown('<h1 style="color:black; font-size:75px;">Projeto SofIA - 498</h1>', unsafe_allow_html=True)
 
-# ---------- IMPORTAÇÃO DAS ABAS DEPOIS DO TÍTULO ----------
-from views import analise_documentos, cadastro_estudantes, conteudo_ia, dashboard_escolar, tradutor
+if "api_key" not in st.session_state:
+    st.session_state.api_key = None
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
 
-# ---------- MENU DE NAVEGAÇÃO ----------
+if not st.session_state.logged_in:
+    login.login()
+    st.stop()
+
 st.sidebar.title("Menu")
 opcao = st.sidebar.radio(
-    "Categorias", 
-    ["Cadastro de Aluno", "Dashboard Escolar", "Análise de Documentos", "Conteúdo com IA", "Tradutor"]
+    "Categorias",
+    ["Cadastro de Aluno", "Dashboard Escolar", "IA com Groq"]
 )
 
 if opcao == "Cadastro de Aluno":
     cadastro_estudantes.show()
 elif opcao == "Dashboard Escolar":
     dashboard_escolar.show()
-elif opcao == "Análise de Documentos":
-    analise_documentos.show()
-elif opcao == "Conteúdo com IA":
-    conteudo_ia.show()
-elif opcao == "Tradutor":
-    tradutor.show()
+elif opcao == "IA com Groq":
+    # ---------- Abas horizontais ----------
+    abas = st.tabs(["Geração de Conteúdo", "Tradutor", "Análise de Documentos"])
+    
+    with abas[0]:
+        conteudo_ia.show()
+    
+    with abas[1]:
+        tradutor.show()
+    
+    with abas[2]:
+        analise_documentos.show()
 
-# ---------- DESIGN ----------
 page_bg = """
 <style>
     /* Fundo */
@@ -61,6 +66,17 @@ page_bg = """
     /* Texto padrão */
     .stApp, .stApp p, .stApp div, .stApp span {
         color: #6A329F;
+    }
+
+    /* Inputs e botões */
+    .stTextInput>div>div>input {
+        background-color: #F0EAF5;
+        color: #6A329F;
+    }
+    div.stButton>button {
+        background-color: #6A329F !important;
+        color: #FFFFFF !important;
+        font-weight: bold;
     }
 </style>
 """
